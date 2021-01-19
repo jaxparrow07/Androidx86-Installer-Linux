c5f462a2cd0aef1499416d3b8a00ceaa95c3fb79
jaxparrow07 <jackandrichardjunior@gmail.com>
2021-01-19 16:39:04 +0530

    Added base view

--- installer.py
+++ installer.py
@@ -1,1 +1,40 @@
- 
+#!/usr/bin/env python3
+
+import sys
+from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication
+from PyQt5.QtGui import QIcon
+
+
+class Example(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        exitAct = QAction(QIcon('exit.png'), '&Exit', self)
+        exitAct.setShortcut('Ctrl+Q')
+        exitAct.setStatusTip('Exit application')
+        exitAct.triggered.connect(qApp.quit)
+
+        self.statusBar()
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAct)
+
+        self.setGeometry(550, 100, 370, 540)
+        self.setWindowTitle('Simple menu')
+        self.show()
+
+
+def main():
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
