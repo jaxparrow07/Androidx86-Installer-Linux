27d6b9b1adea466a3f60d85af38e78c8dcfe56a0
jaxparrow07 <jackandrichardjunior@gmail.com>
2021-01-20 11:00:12 +0530

    Applied theme and added basic design for app

--- installer.py
+++ installer.py
@@ -4,12 +4,13 @@ import sys
 from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication
 from PyQt5.QtGui import QIcon
 
+theme = open('app/MacOS.qss','r')
+
 
 class Example(QMainWindow):
 
     def __init__(self):
         super().__init__()
-
         self.initUI()
 
     def initUI(self):
@@ -18,13 +19,13 @@ class Example(QMainWindow):
         exitAct.setShortcut('Ctrl+Q')
         exitAct.setStatusTip('Exit application')
         exitAct.triggered.connect(qApp.quit)
-
         self.statusBar()
 
         menubar = self.menuBar()
         fileMenu = menubar.addMenu('&File')
         fileMenu.addAction(exitAct)
 
+        self.setStyleSheet(theme.read())
         self.setGeometry(550, 100, 370, 540)
         self.setWindowTitle('Simple menu')
         self.show()
@@ -34,7 +35,6 @@ def main():
     app = QApplication(sys.argv)
     ex = Example()
     sys.exit(app.exec_())
-
 
 if __name__ == '__main__':
     main()
