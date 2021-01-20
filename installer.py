b2956aadb38d9b2a6067bbd6e9d3ae06ea7d7299
jaxparrow07 <jackandrichardjunior@gmail.com>
2021-01-20 14:10:36 +0530

    Added iso Selector

--- installer.py
+++ installer.py
@@ -7,15 +7,43 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 # from PyQt5 import QtWidgets, uic
 
+version_name = 'v0.01 Alpha'
+
 class AboutWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.widget = QWidget(self)
         layout = QVBoxLayout(self)
         layout.addWidget(self.widget)
-        label = QLabel("Sub Window",self)
+        pixmap = QPixmap('app/sg_logo.png')
+        pixmap = pixmap.scaled(150,150,Qt.KeepAspectRatio)
+        Pixmap_label = QLabel(self)
+        Pixmap_label.setPixmap(pixmap)
+        Pixmap_label.setAlignment(Qt.AlignCenter)
+        layout.addWidget(Pixmap_label)
+        # layout.addWidget(pixmap.scaled(100,100,Qt.KeepAspectRatio))
+
+        version_app = QLabel(version_name)
+        version_app.setAlignment(Qt.AlignCenter)
+        version_app.adjustSize()
+
+        layout.addWidget(version_app)
+
+        SG_Name = QLabel('Made by SupremeGamers')
+        SG_Name.setFont(QFont('Arial', 13))
+
+        layout.addWidget(SG_Name)
+        layout.addWidget(QLabel(' '))
+        author_name = QLabel('Programmed by Jaxparrow')
+        author_name.setFont(QFont('Arial',9))
+        author_name.adjustSize()
+        author_name.setAlignment(Qt.AlignCenter)
+        layout.addWidget(author_name)
+        layout.setAlignment(Qt.AlignCenter)
         self.setWindowTitle('About')
         self.setGeometry(570, 190, 330, 330)
+        self.setFixedWidth(330)
+        self.setFixedHeight(330)
 
 class Example(QMainWindow):
 
@@ -30,13 +58,13 @@ class Example(QMainWindow):
         exitAct = QAction(QIcon('exit.png'), '&Exit', self)
         exitAct.setShortcut('Ctrl+Q')
         exitAct.setStatusTip('Exit application')
-        exitAct.triggered.connect(self.func_quit_all_windows)
+        exitAct.triggered.connect(qApp.quit)
         self.statusBar()
 
         selectiso = QAction(QIcon('exit.png'), '&Select iso', self)
         selectiso.setShortcut('Ctrl+F')
         selectiso.setStatusTip('Select iso file')
-        selectiso.triggered.connect(qApp.quit)
+        selectiso.triggered.connect(self.openFileNameDialog)
         self.statusBar()
 
         AboutAct = QAction(QIcon('exit.png'), '&About', self)
@@ -67,8 +95,18 @@ class Example(QMainWindow):
         ############################################################
 
         self.setGeometry(550, 100, 370, 540)
+        self.setFixedWidth(370)
+        self.setFixedHeight(540)
         self.setWindowTitle('Androidx86 Installer')
         self.show()
+
+    def openFileNameDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "",
+                                                  "Android Image Files (*.iso)", options=options)
+        if fileName:
+            print(fileName)
 
     def OpenAbout(self):
         self.abtwin = AboutWindow()
