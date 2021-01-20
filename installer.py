4164602830b2a7780643bebaf8eb027fae2e15df
jaxparrow07 <jackandrichardjunior@gmail.com>
2021-01-20 13:01:38 +0530

    Added About Menu

--- installer.py
+++ installer.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
 
 import sys
-from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication
-from PyQt5.QtGui import QIcon
+# from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+# from PyQt5 import QtWidgets, uic
 
 theme = open('app/MacOS.qss','r')
 
+class AboutWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.widget = QWidget(self)
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.widget)
+        label = QLabel("Sub Window",self)
+        self.setWindowTitle('About')
+        self.setGeometry(570, 190, 330, 330)
 
 class Example(QMainWindow):
 
@@ -14,13 +26,13 @@ class Example(QMainWindow):
         self.initUI()
 
     def initUI(self):
-
+        self.setAttribute(Qt.WA_DeleteOnClose,True)
         # Adding Side Options in Menu
 
         exitAct = QAction(QIcon('exit.png'), '&Exit', self)
         exitAct.setShortcut('Ctrl+Q')
         exitAct.setStatusTip('Exit application')
-        exitAct.triggered.connect(qApp.quit)
+        exitAct.triggered.connect(self.func_quit_all_windows)
         self.statusBar()
 
         selectiso = QAction(QIcon('exit.png'), '&Select iso', self)
@@ -32,7 +44,7 @@ class Example(QMainWindow):
         AboutAct = QAction(QIcon('exit.png'), '&About', self)
         AboutAct.setShortcut('Ctrl+A')
         AboutAct.setStatusTip('About application')
-        AboutAct.triggered.connect(qApp.quit)
+        AboutAct.triggered.connect(self.OpenAbout)
         self.statusBar()
 
         HelpAct = QAction(QIcon('exit.png'), '&Help', self)
@@ -56,11 +68,19 @@ class Example(QMainWindow):
 
         ############################################################
 
-        self.setStyleSheet(theme.read())
         self.setGeometry(550, 100, 370, 540)
-        self.setWindowTitle('Simple menu')
+        self.setWindowTitle('Androidx86 Installer')
         self.show()
 
+    def OpenAbout(self):
+        self.abtwin = AboutWindow()
+        self.abtwin.setAttribute(Qt.WA_DeleteOnClose, True)
+        self.abtwin.show()
+
+    def func_quit_all_windows(self):
+        sys.exit()
+
+        ############################################################
 
 def main():
     app = QApplication(sys.argv)
